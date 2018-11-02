c8d9616d5f98dc8885a1201913c34bf4d7f8682e
Harshit Pandey <harshitpandey678@gmail.com>
2018-11-02 22:00:26 +0530

    webpackchange

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
-var path = require("path");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var DIST_DIR = path.resolve(__dirname, "dist");
-var SRC_DIR = path.resolve(__dirname, "src");
-console.log(DIST_DIR);
-var config = {
-    entry: SRC_DIR + "\\app\\index.js",
-    output: {
-        path: DIST_DIR + "\\app",
-        filename: "bundle.js",
-        publicPath: "\\app\\"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js?/,
-                include: SRC_DIR,
-                loader: "babel-loader",
-                query: {
-                    presets: ["react", "es2015", "stage-2"]
-                }
-            }
-        ]
-    }
-};
-
-module.exports = config;
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.join(__dirname, './dist'),
+		filename: 'index_bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test:/\.css$/,
+				use:['style-loader','css-loader']
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		})
+	]
+}
